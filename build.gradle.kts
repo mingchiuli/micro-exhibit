e7294ac5db917019ce6f178aeb40a301d1b28dc0
mingchiuli <mingchiuli@163.com>
2024-06-30 01:38:10 +0800

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ tasks.named<BootBuildImage>("bootBuildImage") {
 	environment = mapOf("BP_NATIVE_IMAGE_BUILD_ARGUMENTS" to
 			"""
 				-march=compatibility
-				--gc=G1
+				--gc=serial
 				-R:MaxHeapSize=256m
 				-O2
 			""")
